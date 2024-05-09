720443549aff8e6ae496482cb538a9e3e99639d4
talbi rahma <talbirahma73@gmail.com>
2024-05-09 14:20:37 +0100

    patient(labo page)

--- pages/patient/pharmacie.php
+++ pages/patient/pharmacie.php
@@ -342,7 +342,7 @@
   <script src="../../dashboard/assets/js/argon-dashboard.min.js?v=2.0.4"></script>
 </body>
 <div class="col-md-4">
-    <button type="button" class="btn btn-block btn-default mb-3" data-bs-toggle="modal" data-bs-target="#modal-form">Form</button>
+   
     <div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-md" role="document">
         <div class="modal-content">
