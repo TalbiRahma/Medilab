1decd31752b76eadafd15adff97662e6d6b77d07
talbi rahma <talbirahma73@gmail.com>
2024-05-15 19:06:52 +0100

    users profile pages

--- pages/includes/patient/aside.php
+++ pages/includes/patient/aside.php
@@ -16,6 +16,14 @@
             <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
+        <li class="nav-item">
+          <a  href="profile.php">
+            <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+            </div>
+            <span class="nav-link-text ms-1">Profile</span>
+          </a>
+        </li>
         <li class="nav-item" id="accordionRental">
                     <a class=" nav-link " data-bs-toggle="collapse" data-bs-target="#collapseOne"
                         aria-expanded="false" aria-controls="collapseOne">
